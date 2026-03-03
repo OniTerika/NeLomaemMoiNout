33925aab517e4fc576a95b0cdf30d84571c2b851
Onion <onionterraria@gmail.com>
2026-03-03 11:37:28 +0200

    Добавьте файлы проекта.

---
+++ NeLomaemMoiNout/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main()
+        {
+
+
+
+            Random rnd = new Random();
+
+            List<int> list = new List<int>();
+
+            for (int i = 0; i < 50; i++)
+            {
+                list.Add(rnd.Next(-150, 150 + 1));
+                Console.WriteLine($"{list[i]} ");
+            }
+
+            Console.WriteLine("------------------");
+
+            List<int> sorted = list.Where(x => x >= 10 && x <= 99).OrderBy(x => x).ToList();
+
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                Console.Write($"{sorted[i]} ");
+            }
+        }
+    }
+}
